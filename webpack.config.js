aca0e19881c47c790c0b7df3a3d9b9eadd3439a1
Amit Thakur <wondercrazy15@gmail.com>
2023-09-27 17:29:08 +0530

    Reduce  App’s Size with webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+var CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+    // other configurations
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ]
+};
